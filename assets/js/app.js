fe73997ccb8a50c75976055f483ec40c0832376f
ryanyogan <ryanyogan@gmail.com>
2020-01-27 14:01:34 -0800

    more pushes

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,10 +2,15 @@ import { Socket } from "phoenix";
 import "phoenix_html";
 import LiveSocket from "phoenix_live_view";
 import "../css/app.css";
+import ConversationHooks from "./conversation_hooks";
 import ConversationListHooks from "./conversation_list_hooks";
 import CreateConversationFromHooks from "./create_conversation_from_hooks";
 
-let Hooks = { ConversationListHooks, CreateConversationFromHooks };
+let Hooks = {
+  ConversationHooks,
+  ConversationListHooks,
+  CreateConversationFromHooks
+};
 
 let liveSocket = new LiveSocket("/live", Socket, { hooks: Hooks });
 liveSocket.connect();
